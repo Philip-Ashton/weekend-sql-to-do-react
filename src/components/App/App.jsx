dcaa2701d85d6523d2515183a17bde4da2efc4bc
Philip Ashton <philligan1@gmail.com>
2024-05-28 09:22:44 -0500

    started adding a table to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,9 +5,19 @@ function App () {
   return (
     <div>
       <h1>TO DO APP</h1>
+      <table id="tasks">
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>Task</th>
+            <th>Completed</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
-
+console.log ("table created");
 }
 
 export default App
